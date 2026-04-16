ff8899b008dff18bf621449e80f66dd2bdb55cd8
antho <anthonyboisset06@gmail.com>
2026-04-16 11:21:50 +0200

    css: basic style

--- src/views/AgeDeathCalc.vue
+++ src/views/AgeDeathCalc.vue
@@ -7,8 +7,10 @@ const deathAge = ref()
 async function getDeathAge() {
     const userName = name.value
 
-    const result = await fetch(`https://api.agify.io/?name=${userName}`)
+    //const result = await fetch(`https://api.agify.io/?name=${userName}&country_id=ch`)
 
+    deathAge.value = 82
+    return
     if (!result.ok || result.status !== 200) return
 
     const resultAge = await result.json()
@@ -18,25 +20,49 @@ async function getDeathAge() {
 </script>
 
 <template>
-    <h1>Age death calculator</h1>
+    <div class="age-death-calc">
+        <h1>Age of life calculator</h1>
 
-    <div>
+        <div class="input-container">
 
-        <label>
-            <p>Name</p>
-            <input type="text" v-model="name" @keydown.enter="getDeathAge">
-        </label>
-        
-        <button @click="getDeathAge">Calculate</button>
-    </div>
+            <label>
+                <p>Name</p>
+                <input type="text" v-model="name" @keydown.enter="getDeathAge">
+            </label>
+            
+            <button @click="getDeathAge">Calculate</button>
+        </div>
 
 
-    <div>
-        Death: {{ deathAge }}
+        <p v-if="deathAge">
+            End of life at: <strong>{{ deathAge }}</strong> years old
+        </p>
+
     </div>
 </template>
 
 
 <style scoped>
 
+.age-death-calc {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    gap: 20px;
+    padding: 40px;
+}
+
+.input-container label p {
+    margin-bottom: 5px;
+}
+
+h1 {
+    font-weight: bold;
+    font-size: 1.5rem;
+}
+strong {
+    font-weight: bold;
+    color: var(--second-color);
+}
 </style>
